e97bca80e6828118c3e3afd7c6c3d1fe974b25fb
AngeelBarrera <angeelbarrera7@gmail.com>
2018-07-29 14:31:22 -0500

    Se crearon las propiedades y constructores

--- LowesCN/CategoriaProducto.cs
+++ LowesCN/CategoriaProducto.cs
@@ -8,5 +8,26 @@ namespace LowesCN
 {
     public class CategoriaProducto
     {
+        #region Propiedades
+        public int idCategoria { get; private set; }
+        public string nombre { get; private set; }
+        public bool esActivo { get; private set; }
+        public DateTime fechaCreacion { get; private set; }
+        public String descripcion { get; private set; }
+
+        #endregion
+        #region Constructores
+        public CategoriaProducto(int _idCategoria, string _nombre,
+            bool _esActivo, DateTime _fechaCreacion, string _descripcion)
+        {
+            idCategoria = _idCategoria;
+            nombre = _nombre;
+            esActivo = _esActivo;
+            fechaCreacion = _fechaCreacion;
+            descripcion = _descripcion;
+
+
+        }
+        #endregion 
     }
 }
