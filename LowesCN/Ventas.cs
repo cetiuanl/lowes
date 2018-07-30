58ab561e6274758c407187f3be01979690e3ea43
eddy1442 <e.montesinos.s@hotmail.com>
2018-07-29 14:20:32 -1200

    Se complementan las propiedades  y contructores

--- LowesCN/Ventas.cs
+++ LowesCN/Ventas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +12,33 @@ namespace LowesCN
         #region Propiedades
         public int idVenta { get; private set; }
         public int idEmpleado { get; private set; }
-        public int fechaVenta { get; private set; }
+        public DateTime fechaVenta { get; private set; }
         public int estatus { get; private set; }
         public int idCliente { get; private set; }
-        public int tipoPago { get; private set; }
-
-
+        public string tipoPago { get; private set; }
 
         #endregion
 
         #region Contructores
-
+        public Ventas(int _idVenta, int _idEmpleado,DateTime _fechaVenta,
+            int _estatus,int _idCliente,string _tipoPago)
+        {
+            idVenta = _idVenta;
+            idEmpleado = _idEmpleado;
+            fechaVenta = _fechaVenta;
+            estatus = _estatus;
+            idCliente = _idCliente;
+            tipoPago = _tipoPago;
+        }
+        public Ventas(DataRow fila)
+        {
+            idVenta = fila.Field<int>("idVenta");
+            idEmpleado = fila.Field<int>("idEmpleado");
+            fechaVenta = fila.Field<DateTime>("fechaVenta");
+            estatus = fila.Field<int>("estatus");
+            idCliente = fila.Field<int>("idCliente");
+            tipoPago = fila.Field<string>("tipoPago");
+        }
         #endregion
 
         #region Procedimientos y Funciones
