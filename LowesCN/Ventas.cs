36f0aa59ab83f6edab74f1b0988f73157ec3eb8b
eddy1442 <e.montesinos.s@hotmail.com>
2018-07-29 13:29:15 -1200

    Se agragan propiedades

---
+++ LowesCN/Ventas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowesCN
+{
+    public class Ventas
+    {
+        #region Propiedades
+        public int idVenta { get; private set; }
+        public int idEmpleado { get; private set; }
+        public int fechaVenta { get; private set; }
+        public int estatus { get; private set; }
+        public int idCliente { get; private set; }
+        public int tipoPago { get; private set; }
+
+
+
+        #endregion
+
+        #region Contructores
+
+        #endregion
+
+        #region Procedimientos y Funciones
+
+
+        #endregion
+
+    }
+}
