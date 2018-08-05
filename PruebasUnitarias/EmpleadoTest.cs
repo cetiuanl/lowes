7e9b39c12e3ab7529e571cc9ba3e906186b90968
cetiuanl <erolando.avila@aol.com>
2018-08-05 10:37:35 -0500

    Se agrega proyecto para pruebas unitarias

---
+++ PruebasUnitarias/EmpleadoTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class EmpleadoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ PruebasUnitarias/ProductoTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class ProductoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
