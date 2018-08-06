eff664da4f0ff8b02b391a102dff2d7a57321326
lgutierrez88 <edgar.gtz88@gmail.com>
2018-08-05 14:31:17 -1100

    cambios

--- PruebasUnitarias/EmpleadoTest.cs
+++ PruebasUnitarias/EmpleadoTest.cs
@@ -1,14 +1,74 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LowesCN;
+using System.Collections.Generic;
 
 namespace PruebasUnitarias
 {
     [TestClass]
     public class EmpleadoTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestClass]
+        public class RolTest
         {
+            [TestMethod]
+            public void TestGuardar()
+            {
+                bool result = false;
+                string mensaje = "";
+
+                try
+                {
+                    Rol nuevoRol = new Rol(0, "nombre", "descripcion");
+                    nuevoRol.guardar();
+                    result = true;
+                }
+                catch (Exception ex)
+                {
+                    mensaje = ex.Message.ToString();
+                }
+
+                Assert.IsTrue(result, mensaje);
+            }
+
+            [TestMethod]
+            public void TestDesactivar()
+            {
+                bool result = false;
+                string mensaje = "";
+
+                try
+                {
+                    //var rolActual = Rol.traerTodos(true);
+
+                    //Rol.desactivar(rolActual.);
+                    result = true;
+                }
+                catch (Exception ex)
+                {
+                    mensaje = ex.Message.ToString();
+                }
+
+                Assert.IsTrue(result, mensaje);
+            }
+
+            [TestMethod]
+            public void TestTraerTodos()
+            {
+                string mensaje = "";
+
+                List<Rol> roles = null;
+                try
+                {
+                    roles = Rol.traerTodos(false);
+                }
+                catch (Exception ex)
+                {
+                    mensaje = ex.Message.ToString();
+                }
+
+                Assert.IsTrue((roles.Count > 0), mensaje);
+            }
         }
     }
 }
