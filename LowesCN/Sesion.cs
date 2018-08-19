e40df8648a7635046b4322cce39743095ae83fec
cetiuanl <erolando.avila@aol.com>
2018-08-19 13:46:48 -0500

    Nuevo objecto

---
+++ LowesCN/Sesion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowesCN
+{
+    public class Sesion
+    {
+        private static Sesion instancia;
+        public static Sesion getInstancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Sesion();
+                }
+                return instancia;
+            }
+        }
+
+        public Empleado empleadoActual { get; set; }
+        public Rol rolActual { get; set; }
+        public DateTime fechaSesion { get; set; }
+    }
+}
