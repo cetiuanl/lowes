6366a754fab63aeecaf953461b78cffb8260f606
cetiuanl <erolando.avila@aol.com>
2018-08-19 11:52:14 -0500

    mejoras

---
+++ LowesCN/ModoPago.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowesCN
+{
+    public class ModoPago
+    {
+        public static string DisplayMember = "descripcion";
+        public static string ValueMember = "idModoPago";
+
+        public ModoPago(int _idModoPago, string _descripcion)
+        {
+            idModoPago = _idModoPago;
+            descripcion = _descripcion;
+        }
+
+        public int idModoPago { get; set; }
+        public string descripcion { get; set; }
+    }
+}
